7b4e864dc82fb6c90c18c677ce92b07abaf3880c
Rajat Sharma <rajjusharmarajat@gmail.com>
2024-01-25 17:47:30 +0530

    org login page api working

--- Organization/Engine/user/user.service.js
+++ Organization/Engine/user/user.service.js
@@ -1,32 +1,39 @@
-import mongoose from 'mongoose';
-import User from './user.model.js'
-import bcrypt from 'bcrypt'
+import mongoose from "mongoose";
+import User from "./user.model.js";
+import bcrypt from "bcrypt";
 
-export const addUserService = async(req,res)=>{
-    console.log(req.body);
-    const {name,password,email} = req.body;
-    const hashPassword = await bcrypt.hash(password,5);
-    console.log(name,email,hashPassword);
+export const addUserService = async (req, res) => {
+  const { name, password, email } = req.body;
+  console.log(req.body);
+  try {
+    const hashPassword = await bcrypt.hash(password, 10);
     const user = await new User({
-        name: name,
-        password: hashPassword,
-        email: email
-    })
+      name: name,
+      password: hashPassword,
+      email: email,
+    });
     await user.save();
-    res.json({msg:"new user created successfully", User: user})
-}
+    res.json({ msg: "new user created successfully", User: user });
+  } catch (e) {
+    res.json("error", e);
+  }
+};
 
-export const getUserService = async(req,res)=>{
-    const email = req.body.email;
-    const password = req.body.password;
-    const user = await User.findOne({'email': email});
-    if(!user) res.json({msg: "User does not exist"})
-    else{
-       const hashPassword = await user.password;
-       if(await bcrypt.compare(password,hashPassword)) res.json({msg: "Login successful"})
-       else{
-            res.json({msg: "Incorrect password"})
-        }
-        
+export const getUserService = async (req, res) => {
+  const email = req.query.email;
+  const password = req.query.password;
+  try {
+    const user = await User.findOne({ email: email });
+    if (!user) res.json({ msg: "User does not exist" });
+    else {
+      const hashPassword = user.password;
+      if (await bcrypt.compare(password, hashPassword))
+        res.json({ msg: "Login successful" });
+      else {
+        res.json({ msg: "Incorrect password" });
+      }
     }
-}
+  } catch (e) {
+    console.error("error", e);
+  }
+};
--- Organization/Surface/src/components/Login.jsx
+++ Organization/Surface/src/components/Login.jsx
@@ -1,10 +1,40 @@
 import React from "react";
+import axios from "axios";
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: null,
+    };
+  }
 
+    async onSubmit(){
+    const url = "http://localhost:3001/user/get";
+    const userEmail = "rajat@gmail.com";
+    const userPassword = "password";
 
-class Login extends React.Component{
-  render(){
-      return(
-        <div className="container login-container">
+    try {
+      const response = await axios.get(url, {
+        params: {
+          email: userEmail,
+          password: userPassword,
+        },
+        timeout: 5000,
+      });
+      this.setState({ data: response.data });
+    } catch (error) {
+      if (axios.isCancel(error)) {
+        console.log("Request canceled:", error.message);
+      } else {
+        console.error("Error:", error);
+      }
+    }
+  }
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div className="container login-container">
         <div className="row justify-content-center">
           <div className="col-md-6 col-sm-8 col-10">
             <div className="card">
@@ -15,20 +45,32 @@ class Login extends React.Component{
                 <form>
                   <div className="form-group">
                     <label htmlFor="username">Username:</label>
-                    <input type="text" className="form-control" id="username" placeholder="Enter your username" />
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="username"
+                      placeholder="Enter your username"
+                    />
                   </div>
                   <div className="form-group">
                     <label htmlFor="password">Password:</label>
-                    <input type="password" className="form-control" id="password" placeholder="Enter your password" />
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="password"
+                      placeholder="Enter your password"
+                    />
                   </div>
-                  <button type="submit" className="btn btn-primary btn-block">Login</button>
+                  <button type="submit" className="btn btn-primary btn-block">
+                    Login
+                  </button>
                 </form>
               </div>
             </div>
           </div>
         </div>
       </div>
-      )
+    );
   }
 }
 
